29a3d8f41ec397c0e63cd7eb5074058b9fe8810d
Saeed <hassanisaeed19@yahoo.com>
2021-11-30 18:42:08 +0330

    initial commit

---
+++ permissions.py
@@ -0,0 +1,141 @@
+# import jwt
+#
+# from quickstart.settings import JWT_SECRET
+
+SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
+ACCESS_TOKEN_ERROR_MSG = 'Invalid access_token'
+SESSION_TOKEN_ERROR_MSG = 'Invalid session_token'
+SESSION_TOKEN_EXPIRED_MSG = 'Expired session_token'
+PERMISSION_EXPIRED_MSG = 'Endpoint is restricted access'
+
+
+class OperationHolderMixin:
+    def __and__(self, other):
+        return OperandHolder(AND, self, other)
+
+    def __or__(self, other):
+        return OperandHolder(OR, self, other)
+
+    def __rand__(self, other):
+        return OperandHolder(AND, other, self)
+
+    def __ror__(self, other):
+        return OperandHolder(OR, other, self)
+
+    def __invert__(self):
+        return SingleOperandHolder(NOT, self)
+
+
+class SingleOperandHolder(OperationHolderMixin):
+    def __init__(self, operator_class, op1_class):
+        self.operator_class = operator_class
+        self.op1_class = op1_class
+
+    def __call__(self, *args, **kwargs):
+        op1 = self.op1_class(*args, **kwargs)
+        return self.operator_class(op1)
+
+
+class OperandHolder(OperationHolderMixin):
+    def __init__(self, operator_class, op1_class, op2_class):
+        self.operator_class = operator_class
+        self.op1_class = op1_class
+        self.op2_class = op2_class
+
+    def __call__(self, *args, **kwargs):
+        op1 = self.op1_class(*args, **kwargs)
+        op2 = self.op2_class(*args, **kwargs)
+        return self.operator_class(op1, op2)
+
+
+class AND:
+    def __init__(self, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
+
+    def has_permission(self, request, context):
+        return (
+                self.op1.has_permission(request, context) and
+                self.op2.has_permission(request, context)
+        )
+
+    def has_object_permission(self, request, context, obj):
+        return (
+                self.op1.has_object_permission(request, context, obj) and
+                self.op2.has_object_permission(request, context, obj)
+        )
+
+
+class OR:
+    def __init__(self, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
+
+    def has_permission(self, request, context):
+        return (self.op1.has_permission(request, context) or
+                self.op2.has_permission(request, context))
+
+    def has_object_permission(self, request, context, obj):
+        return (self.op1.has_object_permission(request, context, obj) or
+                self.op2.has_object_permission(request, context, obj))
+
+
+class NOT:
+    def __init__(self, op1):
+        self.op1 = op1
+
+    def has_permission(self, request, context):
+        return not self.op1.has_permission(request, context)
+
+    def has_object_permission(self, request, context, obj):
+        return not self.op1.has_object_permission(request, context, obj)
+
+
+class BasePermissionMetaclass(OperationHolderMixin, type):
+    pass
+
+
+class BasePermission(metaclass=BasePermissionMetaclass):
+    """
+    A base class from which all permission classes should inherit.
+    """
+
+    def has_permission(self, request, context):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return True
+
+    def has_object_permission(self, request, context, obj):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return True
+
+
+class IsAuthenticated(BasePermission):
+
+    def has_permission(self, request, context):
+        metadata = dict(context.invocation_metadata())
+        access_token = metadata['access_token']
+        if self.has_access_token_error(access_token):
+            return False
+        has_error, detail = self.has_jwt_error(access_token)
+        if has_error:
+            return False
+        return True
+
+    def has_access_token_error(self, access_token):
+        return not access_token
+
+    def has_jwt_error(self, access_token):
+        # if JWT_SECRET:
+        #     try:
+        #         jwt.decode(access_token,
+        #                    key=JWT_SECRET,
+        #                    algorithms=["HS256"]).get('user_info')
+        #     except jwt.DecodeError:
+        #         return True, SESSION_TOKEN_ERROR_MSG
+        #     except jwt.ExpiredSignatureError:
+        #         return True, SESSION_TOKEN_EXPIRED_MSG
+        return False, None
---
+++ services.py
@@ -0,0 +1,99 @@
+from functools import update_wrapper
+
+import grpc
+from django.db.models.query import QuerySet
+
+from django_grpc_framework.signals import grpc_request_started, grpc_request_finished
+
+
+class Service:
+    permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @classmethod
+    def as_servicer(cls, **initkwargs):
+        """
+        Returns a gRPC servicer instance::
+
+            servicer = PostService.as_servicer()
+            add_PostControllerServicer_to_server(servicer, server)
+        """
+        for key in initkwargs:
+            if not hasattr(cls, key):
+                raise TypeError(
+                    "%s() received an invalid keyword %r. as_servicer only "
+                    "accepts arguments that are already attributes of the "
+                    "class." % (cls.__name__, key)
+                )
+        if isinstance(getattr(cls, 'queryset', None), QuerySet):
+            def force_evaluation():
+                raise RuntimeError(
+                    'Do not evaluate the `.queryset` attribute directly, '
+                    'as the result will be cached and reused between requests.'
+                    ' Use `.all()` or call `.get_queryset()` instead.'
+                )
+
+            cls.queryset._fetch_all = force_evaluation
+
+        class Servicer:
+            def __getattr__(self, action):
+                if not hasattr(cls, action):
+                    return not_implemented
+
+                def handler(request, context):
+                    grpc_request_started.send(sender=handler, request=request, context=context)
+                    try:
+                        self = cls(**initkwargs)
+                        self.request = request
+                        self.context = context
+                        self.action = action
+                        return getattr(self, action)(request, context)
+                    finally:
+                        self.dispatch(request, context)
+                        grpc_request_finished.send(sender=handler)
+
+                update_wrapper(handler, getattr(cls, action))
+                return handler
+
+        update_wrapper(Servicer, cls, updated=())
+        return Servicer()
+
+    def initial(self, request, context):
+        """
+        Runs anything that needs to occur prior to calling the method handler.
+        """
+
+        if hasattr(self, 'permission_classes'):
+            self.check_permissions(request, context)
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        return [permission() for permission in self.permission_classes]
+
+    def check_permissions(self, request, context):
+        """
+        Check if the request should be permitted.
+        Raises an appropriate exception if the request is not permitted.
+        """
+        for permission in self.get_permissions():
+            if not permission.has_permission(request, context):
+                raise ValueError(grpc.StatusCode.UNAUTHENTICATED)
+
+    def dispatch(self, request, context):
+        """
+        `.dispatch()` is pretty much the same as Django's regular dispatch,
+        but with extra hooks for startup, finalize, and exception handling.
+        """
+        self.initial(request, context)
+
+
+def not_implemented(request, context):
+    """Method not implemented"""
+    context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+    context.set_details('Method not implemented!')
+    raise NotImplementedError('Method not implemented!')
