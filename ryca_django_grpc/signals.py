7cb2c2f59d16e2d6bdc5872b976b10851289a26e
Saeed <hassanisaeed19@yahoo.com>
2021-12-01 00:03:32 +0330

    style: formato of lines

--- ryca_django_grpc/signals.py
+++ ryca_django_grpc/signals.py
@@ -1,10 +1,8 @@
 from django.dispatch import Signal
 from django.db import reset_queries, close_old_connections
 
-
 grpc_request_started = Signal()
 grpc_request_finished = Signal()
-
 
 # db connection state managed similarly to the wsgi handler
 grpc_request_started.connect(reset_queries)
